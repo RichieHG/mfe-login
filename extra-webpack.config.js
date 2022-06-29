5c058ca9c392b0b29ee05c29c9586aaa8b79dec7
Ricardo Neftali Hernández Gómez <neftali.hergom@gmail.com>
2022-06-29 08:31:30 -0500

    Feat: Add config webpack to rename bundle

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -2,6 +2,11 @@ const singleSpaAngularWebpack = require('single-spa-angular/lib/webpack').defaul
 
 module.exports = (config, options) => {
   const singleSpaWebpackConfig = singleSpaAngularWebpack(config, options);
+  
+  singleSpaWebpackConfig.output.filename = (pathData) => {
+    return pathData.chunk.name === 'main' ? 'login.[name].js' : '[name].js';
+  }
+  
 
   // Feel free to modify this webpack config however you'd like to
   return singleSpaWebpackConfig;
